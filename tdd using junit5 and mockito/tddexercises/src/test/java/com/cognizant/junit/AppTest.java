10156679b1a412eeeaa009768069de1a435637d1
alphinbiso <alphinbiso@gmail.com>
2026-07-12 23:37:19 +0530

    Added JUnit Exercise 1 - Setting Up JUnit

---
+++ tdd using junit5 and mockito/tddexercises/src/test/java/com/cognizant/junit/AppTest.java
@@ -0,0 +1,13 @@
+package com.cognizant.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testSetup() {
+        assertTrue(true);
+    }
+}
